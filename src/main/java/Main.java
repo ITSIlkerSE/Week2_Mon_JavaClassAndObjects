dcd188acc638deb4dbc920c4a62b6e911f6c60ac
Ilker Yildirim <ITSIlkerSE@gmail.com>
2022-09-05 12:09:02 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student myStudent = new Student();
+
+
+        System.out.println(myStudent.getFirstName());
+        System.out.println(myStudent.getLastName());
+        System.out.println(myStudent.getUniversity());
+
+    }
+
+}
---
+++ src/main/java/Student.java
@@ -0,0 +1,39 @@
+class Student {
+
+    String firstName = "Ilker";
+    String lastName = "Yildirim";
+    String university = "Otto-Friedrich Universität";
+
+    String getFirstName(){
+        return firstName;
+
+    }
+
+    String getLastName(){
+        return lastName;
+
+    }
+
+    String getUniversity(){
+
+        return university;
+    }
+
+
+        void setFirstName (String firstName) {
+            this.firstName = firstName;
+
+        }
+
+        void setLastName (String lastName){
+            this.lastName = lastName;
+
+
+
+        }
+
+        void setUniversity(String university){
+        this.university = university;
+        }
+
+    }
