7afabd494d7ff68fb548bd3dcb2b7b27df16ed1c
Ilker Yildirim <ITSIlkerSE@gmail.com>
2022-09-05 12:27:09 +0200

    New Students

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,10 @@ public class Main {
     public static void main(String[] args) {
 
         Student myStudent = new Student();
+        Student myStudent2 = new Student();
+        Student myStudent3 = new Student();
+
+
 
         myStudent.setFirstName("Ilker");
         myStudent.setLastName("Yildirim");
@@ -12,6 +16,24 @@ public class Main {
         System.out.println(myStudent.getFirstName());
         System.out.println(myStudent.getLastName());
         System.out.println(myStudent.getUniversity());
+
+        myStudent2.setFirstName("Tom");
+        myStudent2.setLastName("Rosenzweig");
+        myStudent2.setUniversity("Universität in Flensburg");
+
+
+        System.out.println(myStudent2.getFirstName());
+        System.out.println(myStudent2.getLastName());
+        System.out.println(myStudent2.getUniversity());
+
+        myStudent3.setFirstName("Manu");
+        myStudent3.setLastName("Wei-Min-Kao");
+        myStudent3.setUniversity("Universität in Bonn");
+
+
+        System.out.println(myStudent3.getFirstName());
+        System.out.println(myStudent3.getLastName());
+        System.out.println(myStudent3.getUniversity());
 
     }
 
